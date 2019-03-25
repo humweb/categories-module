3408d85c44ab1d82f33dd6acb2affd6ef1b70bd0
Ryun Shofner <ryan@humboldtweb.com>
2019-03-25 00:13:49 -0700

    update category form

--- resources/views/categories/form.blade.php
+++ resources/views/categories/form.blade.php
@@ -1,20 +1,15 @@
 @extends('layouts.master')
 @section('content')
-    {!! Form::model($category,['class' => '','url' => $url, 'method' => $method]) !!}
+    {!! Form::model($category, ['class' => '','url' => $url, 'method' => 'post']) !!}
     <div class="card card-default">
         <div class="card-header">
             New Category
-            <div class="pull-right">Parent <span class="label label-default">{{ $parentName }}</span></div>
         </div>
         <div class="card-body">
 
             <div class="form-group">
                 {!! Form::label('label', 'Title', ['class' => 'control-label']) !!}
                 {!! Form::text('title', null, ['class' => 'has-slug form-control','data-slug_target' => "#slug"]) !!}
-            </div>
-            <div class="form-group">
-                {!! Form::label('slug', 'Slug', ['class' => 'control-label']) !!}
-                {!! Form::text('slug', null, ['class' => 'form-control', 'id' => 'slug']) !!}
             </div>
         </div>
         <div class="card-footer">
